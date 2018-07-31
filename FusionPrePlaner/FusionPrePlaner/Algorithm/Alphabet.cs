9ea6b88eab81732824c9d1822d14d8721c151beb
shaocong liu <scliu.chn@gmail.com>
2018-07-31 13:21:40 +0800

    Split Function

---
+++ FusionPrePlaner/FusionPrePlaner/Algorithm/Alphabet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FusionPrePlaner.Algorithm
+{
+    class Alphabet
+    {
+        public const string Letters = "abcdefghijklmnopqrstuvwxyz";
+
+        public static string GetLettersTillTheEndOfAlphabet(String startingFrom)
+        {
+            int count = Letters.Length - Letters.IndexOf(startingFrom);
+            return Letters.Substring(Letters.IndexOf(startingFrom), count);
+        }
+
+        internal static char GetNextAlphabetLetter(char after)
+        {
+            return (char)(after + 1);
+        }
+
+        public static char FirstUpperLetter { get { return char.ToUpper(Letters.First()); } }
+    }
+}
